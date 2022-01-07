12741ac0da07845591b5f5b6eb03e6e7eaba1602
Cam <cameron.miller294@gmail.com>
2022-01-07 13:14:18 -0500

    Update README.md and boilApp.sh
    Added directions to follow after application structure is created.

--- README.md
+++ README.md
@@ -9,4 +9,14 @@ To use this project:
 2. Open the terminal and navigate to this directory
 3. Run `sh boilApp.sh`
 
-This will create the file structure of your full stack application, install dependencies for development and production, and fill in the files with starter code.
+This will create the file structure of your full stack application, install dependencies for development and production, and fill files with starter code.
+
+Once the setup is complete, in the same terminal window, run the following command:
+### `npm run devf`
+
+This will start a frontend development server. Navigate to localhost:9000 in your browser to see the rendered page.
+
+Next, press CTRL+C in the terminal window to terminate the server. Run the following command to bundle your frontend:
+### `npm run build`
+
+Once the build succeeds, you are ready to serve the frontend using Node and Express. Before starting this step, you will have to create an account (free) on MongoDB. Go to https://www.mongodb.com/cloud to do so.
--- boilApp.sh
+++ boilApp.sh
@@ -52,53 +52,54 @@ function writeServerJS {
 }
 
 function writeWebpackConfig {
-  echo "const path = require('path');
-    const HtmlWebPackPlugin = require('html-webpack-plugin');
+  echo "
+  const path = require('path');
+  const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-    module.exports = {
-      mode: 'development',
-      output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-      },
-      resolve: {
-        modules: [path.join(__dirname, 'src'), 'node_modules'],
-        alias: {
-          react: path.join(__dirname, 'node_modules', 'react')
-        }
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader'
-            },
-          },
-          {
-            test: /\.css/,
-            use: [
-              {
-                loader: 'style-loader'
-              },
-              {
-                loader: 'css-loader'
-              }
-            ]
-          }
-        ]
-      },
-      plugins: [
-        new HtmlWebPackPlugin({
-          template: './src/index.html'
-        })
-      ],
-      devServer: {
-        port: 9000,
-        hot: true
+  module.exports = {
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      modules: [path.join(__dirname, 'src'), 'node_modules'],
+      alias: {
+        react: path.join(__dirname, 'node_modules', 'react')
       }
-    }" >> webpack.config.js
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          },
+        },
+        {
+          test: /\.css/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html'
+      })
+    ],
+    devServer: {
+      port: 9000,
+      hot: true
+    }
+  }" >> webpack.config.js
 }
 
 function writeKeyConfig {
