a12d7fe9637d9f76b9a678c12623b37628c95639
Cam <cameron.miller294@gmail.com>
2022-01-12 17:22:51 -0500

    Update boilApp.sh
    Fixed indentation error

--- boilApp.sh
+++ boilApp.sh
@@ -8,39 +8,47 @@ function writeBabelRC {
 }
 
 function writeServerJS {
-  echo "
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const path = require('path');
-  const MongoClient = require('mongodb').MongoClient;
-  // const uri = require('keyconfig').MongoURI;
+  constRequires=(
+    "const express = require('express');"
+    "const bodyParser = require('body-parser');"
+    "const path = require('path');"
+    "const MongoClient = require('mongodb').MongoClient;"
+    "const port = process.env.PORT || 9100;"
+    "const app = express();"
+    ""
+    "// const uri = require('keyconfig').MongoURI;"
+    "// const client = new MongoClient(uri, { useNewUrlParser: true })"
+    ""
+  )
 
-  const port = process.env.PORT || 9100;
-  const app = express();
-  // const client = new MongoClient(uri, { useNewUrlParser: true })
-  " >> server.js
-
-  echo "
-  /*
-  async function connectToDB() {
-    await client.connect( err => {
-      err ? console.log(err) : console.log('Connected to database');
-    });
-  }
-  connectToDB();
-  */
-
-  const pathToBuild = path.join(__dirname, 'build');
-  " >> server.js
+  dbConnection=(
+    "async function connectToDB() {"
+    "\tawait client.connect( err => {"
+    "\t\terr ? console.log(err) : console.log('Connected to database');"
+    "\t});"
+    "}"
+    "connectToDB();"
+    ""
+    "const pathToBuild = path.join(__dirname, 'build');"
+    ""
+  )
 
   appMethods=(
     "app.use(bodyParser.urlencoded({ extended: true }));"
     "app.use(express.static(pathToBuild));"
-    "app.get('/', (req, res) => {
-      res.sendFile(pathToBuild, 'index.html');
-    });"
+    "app.get('/', (req, res) => {"
+    "\tres.sendFile(pathToBuild, 'index.html');"
+    "});"
     "app.listen(port, () => console.log('Listening on ' + port));"
   )
+
+  for const in "${constRequires[@]}"; do
+    echo $const >> server.js
+  done
+
+  for cdb in "${dbConnection[@]}"; do
+    echo $cdb >> server.js
+  done
 
   for am in "${appMethods[@]}"; do
     echo $am >> server.js
@@ -48,61 +56,64 @@ function writeServerJS {
 }
 
 function writeWebpackConfig {
-  echo "
-  const path = require('path');
-  const HtmlWebPackPlugin = require('html-webpack-plugin');
+  echo "const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-  module.exports = {
-    mode: 'development',
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js'
-    },
-    resolve: {
-      modules: [path.join(__dirname, 'src'), 'node_modules'],
-      alias: {
-        react: path.join(__dirname, 'node_modules', 'react')
-      }
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          },
-        },
-        {
-          test: /\.css/,
-          use: [
-            {
-              loader: 'style-loader'
-            },
-            {
-              loader: 'css-loader'
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: './src/index.html'
-      })
-    ],
-    devServer: {
-      port: 9000,
-      hot: true
+module.exports = {
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react')
     }
-  }" >> webpack.config.js
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    port: 9000,
+    hot: true
+  }
+}" >> webpack.config.js
 }
 
 function writeKeyConfig {
-  echo "
-  module.exports = {
-    MongoURI: 'mongodb+srv://user:password@cluster0.0wmx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  }" >> keyconfig.js
+  kConfig=(
+    "module.exports = {"
+    "\tMongoURI: 'mongodb+srv://user:password@cluster0.0wmx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'"
+    "}"
+  )
+  for kc in "${kConfig[@]}"; do
+    echo $kc >> keyconfig.js
+  done
 }
 
 function writeSrcFiles {
@@ -126,13 +137,13 @@ function writeSrcFiles {
   render(<App />, document.getElementById('root'));" >> src/index.js
 
   echo "
-  const App = () => {
-    return (
-      <h1>Awesome frontend here! If you see this, you're doing great.</h1>
-    )
-  }
+const App = () => {
+  return (
+    <h1>Awesome frontend here! If you see this, you're doing great.</h1>
+  )
+}
 
-  export default App;" >> src/app.js
+export default App;" >> src/app.js
 }
 
 function updatePackageJSON {
@@ -145,17 +156,17 @@ function updatePackageJSON {
 }
 
 function writeFiles {
-  writeBabelRC
+  # writeBabelRC
   writeServerJS
-  writeWebpackConfig
-  writeKeyConfig
-  writeSrcFiles
-  updatePackageJSON
+  # writeWebpackConfig
+  # writeKeyConfig
+  # writeSrcFiles
+  # updatePackageJSON
   echo "Finished writing files"
 }
 
 function boilApp {
-  echo "Setting up full stack (MERN) app with NPM..."
+  echo "Setting up full stack (MERN) app with npm..."
 
   dependencies=( react react-dom express mongodb body-parser )
   devDependencies=(
@@ -181,7 +192,7 @@ function boilApp {
   globalFiles=( server.js webpack.config.js keyconfig.js .babelrc .gitignore )
   srcFiles=( app.js index.js style.css index.html )
 
-  for di in "${directoreis[@]}"; do
+  for di in "${directories[@]}"; do
     mkdir $di
   done
 
@@ -202,4 +213,6 @@ function boilApp {
   echo "Application setup completed"
 }
 
-boilApp
+# boilApp
+
+writeFiles
