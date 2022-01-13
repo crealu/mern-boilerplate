c82bf6d38bf40de4328df6d14575cd49f69622c3
Cam <cameron.miller294@gmail.com>
2022-01-13 18:31:17 -0500

    Added sample directory
    The test folder contains the file structure that gets created.

---
+++ test/keyconfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+	MongoURI: 'mongodb+srv://user:password@cluster0.0wmx5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ test/src/app.js
@@ -0,0 +1,7 @@
+const App = () => {
+	return (
+		<h1>Awesome frontend here! If you see this, you're doing great.</h1>
+	)
+}
+
+export default App;
