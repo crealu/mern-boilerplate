c636ba93fe8492d61626de53164362f53896f764
Cam <cameron.miller294@gmail.com>
2022-01-14 17:34:37 -0500

    Update saveCmd.sh
    Custom terminal command creation script finished

--- boilApp.sh
+++ boilApp.sh
@@ -227,4 +227,4 @@ function boilApp {
   echo "Application setup completed"
 }
 
-boilApp
+boilApp # main
--- saveCmd.sh
+++ saveCmd.sh
@@ -3,12 +3,26 @@
 # Creates a custom terminal command for boilApp to be used anywhere in the terminal.
 
 function saveCmd {
-  touch .boilApp.sh
   # copy boilApp.sh into .boilApp.sh
-  # move boilApp.sh to home directory
+  cp boilApp.sh .boilApp.sh
+
+  # comment out boilApp function call
+  sed -i.bak '/boilApp # main/d' .boilApp.sh
+
+  # delete backup file
+  rm .boilApp.sh.bak
+
+  # move .boilApp.sh to home directory
+  mv .boilApp.sh ~/
+
   # change to /etc
+  cd ~ && cd .. && cd ../etc
+
   # add source ~/.boilApp.sh to zshrc file
-  echo "Terminal command created\nType 'boilApp' to use"
+  echo '\nsource ~/.boilApp.sh' >> zshrc
+
+  # notify user
+  echo "\nTerminal command created\nType 'boilApp' to use"
 }
 
 saveCmd
