0fe4117023fdf6b2676c51d097efdc50130e59de
Cam <cameron.miller294@gmail.com>
2022-01-31 16:43:28 -0500

    Update boilApp.sh

--- boilApp.sh
+++ boilApp.sh
@@ -178,17 +178,7 @@ function writeFiles {
   updatePackageJSON
   echo "Finished writing files"
 
-  if [ $# -eq 0 ]
-    then
-      echo "Initialized with basic file structure"
-  elif [ $1 = bfs ]
-    then
-      echo "Initialized with backend file structure"
-      mkdir config
-      mkdir models
-      mkdir routes
-      mkdir views
-  fi
+
 }
 
 function boilApp {
@@ -229,6 +219,31 @@ function boilApp {
   for sf in "${srcFiles[@]}"; do
     touch src/$sf
   done
+
+  if [ $# -eq 0 ]
+    then
+      echo "Initialized with basic file structure"
+  elif [ $1 = bfs ]
+    then
+      echo "Initialized with backend file structure"
+      mkdir config
+      mkdir models
+      mkdir routes
+      mkdir views
+
+      configfiles=( keys.js passport.js auth.js )
+      for cf in "${configFiles[@]}"; do
+        touch config/$cf
+      done
+
+      touch models/user.js
+      touch routes/index.js
+
+      viewFiles=( index.ejs signup.ejs login.ejs )
+      for vf in "${viewFiles[@]}"; do
+        touch views/$vf
+      done
+  fi
 
   echo "Finished generating configuration and source files"
   echo "Writing files..."
