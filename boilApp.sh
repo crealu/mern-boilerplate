e9be5c481e43a349fbee9c7cc7f8bf8778043385
Cam <cameron.miller294@gmail.com>
2022-02-03 01:07:40 -0500

    Refactored structure.sh

--- boilApp.sh
+++ boilApp.sh
@@ -8,21 +8,11 @@ source ./src/files.sh
 
 function initApplication {
   echo "Setting up full stack (MERN) app with npm..."
-  d=( hello hey sup )
-  f=( oy me ha do )
 
-  function forList {
-    arr=("$@")
-    for l in $arr; do
-      echo $l
-    done
-  }
-  forList "${d[@]}"
-
-  # npm init -y
-  # installDependencies
-  # createStructure
-  # writeFiles
+  npm init -y
+  installDependencies
+  createStructure
+  writeFiles
 
   echo "Application setup complete"
 }
--- src/structure.sh
+++ src/structure.sh
@@ -3,32 +3,32 @@
 # Creates the application structure of folders and files
 
 function createStructure {
-  globalFiles=( server.js webpack.config.js .babelrc .gitignore )
-  for gf in "${globalFiles[@]}"; do
-    touch $gf
-  done
-
   echo "Initialized with backend file structure"
   directories=( build public src config models views routes)
   srcFiles=( app.js index.js style.css index.html )
-  configfiles=( keys.js passport.js auth.js )
+  configFiles=( keys.js passport.js auth.js )
   viewFiles=( index.ejs signup.ejs login.ejs resetPassword.ejs )
+  globalFiles=( server.js webpack.config.js .babelrc .gitignore )
 
   for di in "${directories[@]}"; do
     mkdir $di
   done
 
-  for cf in "${configFiles[@]}"; do
-    touch config/$cf
-  done
+  function createFiles() {
+    files=("$@")
+    for f in "${files[@]}"; do
+      if [ "$1" == "" ] ; then
+        echo "$f"
+      elif [ "$1" != "$f" ] ; then
+        echo "$1"/"$f"
+      fi
+    done
+  }
 
-  for vf in "${viewFiles[@]}"; do
-    touch views/$vf
-  done
-
-  for sf in "${srcFiles[@]}"; do
-    touch src/$sf
-  done
+  createFiles "" "${globalFiles[@]}"
+  createFiles "config" "${configFiles[@]}"
+  createFiles "views" "${viewFiles[@]}"
+  createFiles "src" "${srcFiles[@]}"
 
   touch models/user.js
   touch routes/index.js
